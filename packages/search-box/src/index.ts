54cc8eee3da955265da8e3de1109e2b266a66718
liukun <953831480@qq.com>
2025-07-25 17:21:34 +0800

    Update package.json
    修改依赖版本

--- packages/search-box/src/index.ts
+++ packages/search-box/src/index.ts
@@ -1,6 +1,8 @@
 import { App } from 'vue'
 import { watch } from 'vue'
 import TinySearchBox from './index.vue'
+import TinySearchBoxFirstLevelPanel from './components/first-level-panel.vue'
+import TinySearchBoxSecondLevelPanel from './components/second-level-panel.vue'
 import zhCN from './utils/zh_CN'
 import enUS from './utils/en_US'
 import './index.less'
@@ -19,6 +21,6 @@ export const t = (key) => {
     : zhCN?.[array?.[0]]?.[array?.[1]]?.[array?.[2]]
 }
 
-export { zhCN, enUS }
+export { zhCN, enUS, TinySearchBoxFirstLevelPanel, TinySearchBoxSecondLevelPanel }
 
 export default TinySearchBox
