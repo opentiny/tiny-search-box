d6a5abaf6531b8ac59d45898cbfab94d01ef0106
LiangGuanhui <liangguanhui0117@qq.com>
2025-10-28 16:50:37 +0800

    feat: add size (#18)

---
+++ packages/docs/search-box/size.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { dataSource } from './data-source'
+
+const tags = ref([])
+</script>
+
+<template>
+  <ClientOnly>
+    <tiny-search-box v-model="tags" :items="dataSource" />
+    size="small"
+    <tiny-search-box v-model="tags" :items="dataSource" size="small" />
+  </ClientOnly>
+</template>
